5cea4b430d5c3621916700bcf744004bba661c8d
EstebanFerS <151755309+EstebanFerS@users.noreply.github.com>
2024-03-24 17:59:46 -0600

    Prototipo Vera
    Una pequeña simulación de como será VERA

---
+++ Prototipo/code.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".botonVotar").addEventListener("click", function() {
+        window.location.href = "votoenviado.html";
+    });
+});
